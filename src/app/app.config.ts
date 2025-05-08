1e94fed19199427c44a6c72cc0e647286a2807e3
Mircus58 <spagnolo_mg@outlook.com>
2025-05-08 17:03:39 +0200

    ajout de ionic et debut firebase

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,15 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideIonicAngular } from '@ionic/angular/standalone';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getStorage, provideStorage } from '@angular/fire/storage';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideIonicAngular({}), provideFirebaseApp(() => initializeApp({ projectId: "ecomore-f30a5", appId: "1:509475421179:web:41e3b6710383882cf5a95e", storageBucket: "ecomore-f30a5.firebasestorage.app", apiKey: "AIzaSyD6D10LY3i9DUw7EYaHIFk5Q4dAD4A01CI", authDomain: "ecomore-f30a5.firebaseapp.com", messagingSenderId: "509475421179" })),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+    provideStorage(() => getStorage())]
 };
